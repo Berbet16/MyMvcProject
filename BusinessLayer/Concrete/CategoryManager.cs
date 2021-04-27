03bf931858b9f3ddacba7b3d46dd4bd7d2099626
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-04-27 18:41:43 +0300

    Step-2

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -18,15 +18,15 @@ namespace BusinessLayer.Concrete
         }
         public void CategoryAddBL(Category p)
         {
-            if(p.CategoryName == "" || p.CategoryName.Length<=3 || 
-               p.CategoryDescription =="" || p.CategoryName.Length>=51 )
-            {
-                //hata mesajı
-            }
-            else
-            {
+            //if(p.CategoryName == "" || p.CategoryName.Length<=3 || 
+            //   p.CategoryDescription =="" || p.CategoryName.Length>=51 )
+            //{
+            //    //hata mesajı
+            //}
+            //else
+            //{
                 repo.Insert(p);
-            }
+            //}
         }
 
 
--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,29 @@ namespace MvcProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        //Category dan verileri çekmemiz için yeni nesne oluşturduk
+        CategoryManager cm = new CategoryManager();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetAllBL(); //category manager sınıfından verileri aldık.
+
+            return View(categoryvalues); //bize geriye aldığımız verileri döndür
+        }
+
+        public ActionResult AddCategory(Category cat)
+        {
+            cm.CategoryAddBL(cat); //catorgy managerden yer alan metotu kullandık.
+
+            //ekleme işleminden sonra "  "   metota yönlendir.
+            return RedirectToAction("GetCategoryList");  
+
+
         }
     }
 }
