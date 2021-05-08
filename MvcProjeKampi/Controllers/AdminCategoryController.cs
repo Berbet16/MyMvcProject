4ec1fcb09b046bd55233721d3e9c434d28d54601
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-05-08 18:25:30 +0300

    Step - 5 ( Tema Eklendi)

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidatior categoryValidator = new CategoryValidatior();
+            ValidationResult results = categoryValidator.Validate(p); //results değişkeni gelen değerlere göre kontrol ediyor
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+
+            }
+            return View();
+        }
+    }
+}
