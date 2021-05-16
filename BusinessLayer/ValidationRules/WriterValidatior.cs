3443f517f6206c9c38f7e17c6dac8c8bf74ff106
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-05-16 14:32:07 +0300

    Step - 44

--- BusinessLayer/ValidationRules/WriterValidatior.cs
+++ BusinessLayer/ValidationRules/WriterValidatior.cs
@@ -18,17 +18,12 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar Soyadını Boş Geçemezsiniz.");
             RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("En Az 3 Karakter Girişi Yapın.");
             RuleFor(x => x.WriterSurname).MaximumLength(20).WithMessage("En Fazla 20 Karakter Girişi Yapın.");
-            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Kısmını Boş Geçemezsiniz.");
-            RuleFor(x => x.WriterAbout).Must(MustBeA).WithMessage("Hakkında Kısmında A Harfi Olmalıdır.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında Boş Geçemezsiniz").Must(x => x.Contains('A')).WithMessage("Hakkında bilgisinde A karakteri olmak zorundadır.");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail Adresini Boş Geçemezsiniz");
             RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Mail Adresi Türünde Olmalıdır.");
             RuleFor(x => x.WriterImage).NotEmpty().WithMessage("Fotoğraf Kısmını Boş Geçemezsiniz");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifreyi Boş Geçemezsiniz");
             RuleFor(x => x.WriterPassword).MinimumLength(3).WithMessage("En Az 3 Karakter Girişi Yapınız.");
-        }
-        private bool MustBeA(string arg)
-        {
-            return arg.Contains("A");
         }
     }
 }
