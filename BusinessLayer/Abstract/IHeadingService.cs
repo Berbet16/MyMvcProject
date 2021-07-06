29edddd1858ef8e6600b27d768b3f98369d7b71d
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-07-06 14:19:42 +0300

    6 - 06 - 2021 / Yazar - Başlık İşlemleri Tamamlandı.

--- BusinessLayer/Abstract/IHeadingService.cs
+++ BusinessLayer/Abstract/IHeadingService.cs
@@ -10,6 +10,7 @@ namespace BusinessLayer.Abstract
     public interface IHeadingService
     {
         List<Heading> GetList();
+        List<Heading> GetListByWriter();
         void HeadingAdd(Heading heading);
         Heading GetById(int id); //sadece id e göre işlem yapılacak
         void HeadingDelete(Heading heading);
---
+++ EntityLayer/Concrete/Skill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Skill
+    {
+        [Key]
+        public int SkillId { get; set; }
+        public string SkillName { get; set; }
+        public int SkillPercent { get; set; }
+        public int SkillValue { get; set; }
+    }
+}
