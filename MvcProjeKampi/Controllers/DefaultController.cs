3cbea6124d67c9f95eeb4290565c20f3a20e8ba5
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-07-14 15:14:23 +0300

    14 - 07 - 2021 /Vitrin Sayfası oluşturuldu.

---
+++ MvcProjeKampi/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+
+        public ActionResult Headings()
+        {
+            var headinglist = hm.GetList();
+            return View(headinglist);
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
